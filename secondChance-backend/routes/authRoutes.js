0fb42025385d5f497aca7c17741e9d50d3af1086
Ahmad Al-Bourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2025-04-04 08:17:59 -0400

    add updated api

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
 
+const { body, validationResult } = require('express-validator');
+
 dotenv.config();
 
 
@@ -86,13 +88,76 @@ router.post('/login', async (req, res) => {
 
             const authtoken = jwt.sign(payload, process.env.JWT_SECRET)
 
-            logger.error('User Login successfuly!');
+            logger.info('User Login successfuly!');
             res.json({ authtoken, userName, userEmail });
 
         } else {
             // Send appropriate message if the user is not found
             return res.status(404).json({ error: 'User not found' });
         }
+
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
+    }
+});
+
+
+
+router.put('/update', async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        const db = await connectToDatabase()
+        const collection = db.collection('users')
+
+        const existingUser = await collection.findOne({ email });
+
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+    
+        // Update the user credentials in the database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+
+        // Create JWT authentication with `user._id` as a 
+        // payload using the secret key from the .env file
+
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, process.env.JWT_SECRET);
+        
+        console.log("The Name is Updated");
+        logger.info('User Updated successfuly!');
+
+        res.json({ authtoken });
 
     } catch (e) {
         return res.status(500).send('Internal server error');
